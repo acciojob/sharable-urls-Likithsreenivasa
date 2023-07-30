afae8b7b37f81da6fd43e8280e570187936f1c5e
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-07-30 16:21:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // your code here
+let h3=document.getElementById("add");
+let value1=h3.innerText;
+function addTheme(){
+	let add=document.getElementById("add");
+	let name=document.getElementById("name");
+	let year=document.getElementById("year")
+	let nameValue=name.value;
+	let yearValue=year.value;
+	if(nameValue!=="" && yearValue!=="")
+	{
+		add.innerText=value1+`?name=${nameValue}&year=${yearValue}`
+		
+	}
+	else if(nameValue==="")
+	{
+		add.innerText=value1+`?year=${yearValue}`
+	}
+	else if(yearValue==="")
+	{
+		add.innerText=value1+`?name=${nameValue}`
+	}
+	
+	
+}
+
+
+
+
+
+const submit=document.getElementById("button");
+submit.addEventListener("click",addTheme)
